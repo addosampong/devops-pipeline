30008dd5e8498f8b5523dc20659c180f63464ac1
addosampong <evans@dataceum.com>
2026-05-04 15:45:28 +0000

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello, DevOps Pipeline!');
+console.log('I am learning CI/CD with GitHub Actions');
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Starting DevOps Pipeline Test...');
+setTimeout(() => {
+    console.log('DevOps Pipeline Test Completed!');
+}, 2000);
+console.log('All tests successfully executed!');
